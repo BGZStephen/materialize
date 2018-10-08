fcccd56fb33a5eb4d1945adcedad31ce441c3763
Stephen Wright <stephen@getawriggleon.com>
2018-10-08 12:49:00 +0100

    project imit, font-awesome definition, font inclusions and file layout

---
+++ src/helpers/font-awesome.js
@@ -0,0 +1,4 @@
+// import {library} from '@fortawesome/fontawesome-svg-core';
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+// import icons from FontAwesomeIcon and add them using Library.add(icon)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import './styles/main.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/app/app.jsx';
 import * as serviceWorker from './serviceWorker';
+import './helpers/font-awesome';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
